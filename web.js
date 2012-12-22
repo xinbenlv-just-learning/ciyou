b916c693f04d1468b389e76e2bda2c996042e509
Zainan Victor Zhou <zzn@zzn.im>
2012-12-22 09:01:00 -0800

    switch to express

--- web.js
+++
@@ -1,7 +0,0 @@
-var port = process.env.PORT || 8080;
-var connect = require('connect');
-console.log("listening on " + port);
-
-connect.createServer(
-    connect.static(__dirname)
-).listen(port);
